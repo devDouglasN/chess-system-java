57a027f2e2f81df7e0259a34616135be3103a2cb
Douglas <douglas_timaofiel@hotmail.com>
2023-05-05 12:17:02 -0300

    Special move - Castling

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -53,10 +53,10 @@ public class Rook extends ChessPiece{
 		}
 		
 		// below
-		p.setValues(position.getRow() - 1, position.getColumn());
+		p.setValues(position.getRow() + 1, position.getColumn());
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() - 1);
+			p.setRow(p.getRow() + 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
 			mat[p.getRow()][p.getColumn()] = true;
